3fe8772c00ab3904a3ec816ce3011b1fef094d4d
Lucas <felipelucasmoura350@gmail.com>
2026-07-18 22:38:31 -0300

    feat: adiciona RabbitMQConfig no payment-service (payment.queue vinculada a pedido.criado)

---
+++ payment-service/src/main/java/com/orderflow/payment_service/config/RabbitMQConfig.java
@@ -0,0 +1,36 @@
+package com.orderflow.payment_service.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public Queue paymentQueue() {
+        Queue queue = new Queue("payment.queue");
+        return queue;
+    }
+
+    @Bean
+    public FanoutExchange pedidoCriadoExchange() {
+        FanoutExchange exchange = new FanoutExchange("pedido.criado");
+        return exchange;
+    }
+
+    @Bean
+    public Binding biding(Queue paymentQueue, FanoutExchange pedidoCriadoExchange) {
+        return BindingBuilder.bind(paymentQueue).to(pedidoCriadoExchange);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+}
